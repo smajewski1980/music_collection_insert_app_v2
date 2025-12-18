800f58d6a24feeccff800c9f704b7804ac085603
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-12-18 14:24:09 -0500

    got the records done in the current locations script

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,106 @@
+function customSort(a, b) {
+  return a.localeCompare(b, undefined, {
+    numeric: true,
+    sensitivity: "base",
+  });
+}
+
+function getMostCurrentSubLoc(array) {
+  const misc = [];
+  const classical = [];
+  const comedy = [];
+  const country = [];
+  const jazz = [];
+  const unopened = [];
+  const pop = [];
+  const soundtracks = [];
+  const various = [];
+  // sort out by category
+  array.forEach((loc) => {
+    currLoc = loc.split(" ")[1];
+    if (currLoc === "Classical") {
+      classical.push(loc);
+    } else if (currLoc === "Country") {
+      country.push(loc);
+    } else if (currLoc === "Comedy") {
+      comedy.push(loc);
+    } else if (currLoc === "Jazz") {
+      jazz.push(loc);
+    } else if (currLoc === "NEW") {
+      unopened.push(loc);
+    } else if (currLoc === "Pop/Rock") {
+      pop.push(loc);
+    } else if (currLoc === "Soundtracks") {
+      soundtracks.push(loc);
+    } else if (currLoc === "Various") {
+      various.push(loc);
+    } else {
+      misc.push(loc);
+    }
+  });
+
+  // get current vals
+  const currClassical = classical.sort(customSort);
+  const currCountry = country.sort(customSort);
+  const currComedy = comedy.sort(customSort);
+  const currJazz = jazz.sort(customSort);
+  const currUnopened = unopened.sort(customSort);
+  const currPop = pop.sort(customSort);
+  const currSoundtracks = soundtracks.sort(customSort);
+  const currVarious = various.sort(customSort);
+
+  // return array with the vals
+  return [
+    currClassical.at(-1),
+    currCountry.at(-1),
+    currComedy.at(-1),
+    currJazz.at(-1),
+    currUnopened.at(-1),
+    currPop.at(-1),
+    currSoundtracks.at(-1),
+    currVarious.at(-1),
+    ...misc,
+  ];
+}
+
+function getMostCurrentRecordsLoc(array) {
+  const rec45s = [];
+  const rec33s = [];
+  const rec78s = [];
+  const misc = ["Herb Alpert Records", '12" Singles']; //<-- when we switch over to this app, we will change the name of the loc to start with 33s in the db
+  // distibute the values to the appropriate array
+  array.forEach((loc) => {
+    if (loc.location.includes("45s ")) {
+      rec45s.push(loc.location);
+    }
+    if (loc.location.includes("33s ")) {
+      rec33s.push(loc.location);
+    }
+    if (loc.location.includes("78s ")) {
+      rec78s.push(loc.location);
+    }
+  });
+
+  // sort the sortables and get currents
+  const sorted45s = rec45s.sort((a, b) => {
+    return a.localeCompare(b, undefined, {
+      numeric: true,
+      sensitivity: "base",
+    });
+  });
+  const sorted78s = rec78s.sort((a, b) => {
+    return a.localeCompare(b, undefined, {
+      numeric: true,
+      sensitivity: "base",
+    });
+  });
+  const curren33sLocs = getMostCurrentSubLoc(rec33s);
+  const current45sLoc = sorted45s.at(-1);
+  const current78sLoc = sorted78s.at(-1);
+
+  return [...curren33sLocs, current45sLoc, current78sLoc, ...misc];
+}
+
 function getMostCurrentTapeLoc(array) {
   const eightTracks = [];
   const reelToReel = [];
@@ -47,7 +150,7 @@ function getMostCurrentLoc(array, format) {
       return getMostCurrentTapeLoc(array);
       break;
     case "records":
-      console.log("records");
+      return getMostCurrentRecordsLoc(array);
       break;
     case "cds":
       console.log("cds");
@@ -70,11 +173,12 @@ function processLocations(data) {
   const cdCompsLocs = data.cdComps;
   const cdSinglesLocs = data.cdSingles;
   const currTapeLoc = getMostCurrentLoc(tapesLocs, "tapes");
-  getMostCurrentLoc(recordsLocs, "records");
+  const currRecordsLocs = getMostCurrentLoc(recordsLocs, "records");
   getMostCurrentLoc(cdsLocs, "cds");
   getMostCurrentLoc(cdSinglesLocs, "cdSingles");
   getMostCurrentLoc(cdCompsLocs, "cdComps");
-  console.log(currTapeLoc);
+  console.log("curr tapes", currTapeLoc); // <--these will be the final result to use for the ui
+  console.log("curr records", currRecordsLocs);
 }
 
 async function getLocations() {
